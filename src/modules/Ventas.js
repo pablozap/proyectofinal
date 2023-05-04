ab745bb206bc639ddff52b0ed81a19ad301cf08d
Juan Pablo Zapata Gallego <juan.zapata046@pascualbravo.edu.co>
2023-05-03 21:54:00 -0500

    Error en array modulo productos arreglado

--- src/modules/Ventas.js
+++ src/modules/Ventas.js
@@ -1,21 +1,31 @@
-import { useState } from "react";
 import './Ventas.css';
+import { useState } from "react";
 import {actuales} from './Productos';
 import Select from "react-select";
+export let actualGanancia = 0;
 export const Ventas = () => {
-    const [cantidad, setCantidad] = useState(null);
+    const [cantidad, setCantidad] = useState(0);
+    const [seleccionado, setSeleccionado] = useState({});
 
+    const posicion = () => {
+        for (let i = 0; i < actuales.length; i++) {
+            if (actuales[i].name == seleccionado.name) {
+                return i;
+            }
+        }}
+    
     const Venta = () => {
-        let posicion = actuales.indexof(Select.target.value);
-        actuales[posicion].quantity -= cantidad;
-        console.log(actuales[posicion].quantity);
+        let ubicacion = posicion();
+        actualGanancia += parseInt(actuales[ubicacion].price) * cantidad;
+        actuales[ubicacion].quantity -= cantidad;
     }
 
     return (           
         <div className="Ventas-contenedor">
             <h1>Ventas</h1>
             <Select
-                options={actuales.map(objeto => ({value: objeto, label: objeto.name}))}
+                options={actuales.map(objeto => ({value: objeto, label: objeto.nombre}))}
+                onChange={(value) => setSeleccionado(value)}
             />
             <input type="number" placeholder="Cantidad" onChange={(e) => setCantidad(e.target.value)} />
             <button onClick={(e) => Venta()}>Guardar</button>
--- src/modules/productos.js
+++ src/modules/productos.js
@@ -1,27 +1,20 @@
 import React  from "react";
 import { useState } from "react";
 import './Productos.css';
+export let actuales = null;
 export const Productos = () => {
     const [nombre, setNombre] = useState("");
     const [precio, setPrecio] = useState(0);
     const [cantidad, setCantidad] = useState(0);
     const [productos, setProductos] = useState([]);
-    const agregarProducto = (name, price, quantity) =>{
-        let objeto={
-            name, price, quantity
-        };
-        setProductos([...productos, objeto]);
-        console.log(productos);
-        actuales = productos;
-        console.log(actuales);
-    }
+    actuales = productos;
     return (     
         <div className="productos-contenedor">
             <h1>Mis Productos</h1>
             <ul>
                 <li>
                 <label htmlFor="nombre">Nombre del Producto</label>
-                <input type="text" onChange={(e) => setNombre(e.target.value)} id="nombre"/>
+                <input type="text" onChange={(e) => setNombre(e.target.value) } id="nombre"/>
                 </li>
                 <li>
                 <label htmlFor="precio">Precio</label>
@@ -29,11 +22,12 @@ export const Productos = () => {
                 </li>
                 <li>
                 <label htmlFor="cantidad">Cantidad</label>
-                <input type="number" onChange={(e) => setCantidad(e.target.value)} id="cantidad"/>
+                <input type="number" onChange={(e) => (setCantidad(e.target.value))} id="cantidad"/>
                 </li>
             </ul>
-            <button onClick={() => agregarProducto(nombre, precio, cantidad)}>Guardar</button>
+            <button onClick={() => setProductos([...productos, {nombre, precio, cantidad}])} >Guardar</button>
+            <button onClick={() => console.log(actuales)}>Ver productos</button>
+            
         </div>
     )
 }
-export let actuales;
